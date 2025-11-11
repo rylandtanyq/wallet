7c1f002a58ddf6e88579ba6b6a6d798588d19628
Tyq_cool <896953946@qq.com>
2025-11-11 14:25:53 +0800

    fix: update widget test for MyApp with ProviderScope

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,14 +6,16 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:untitled1/i18n/strings.g.dart';
 
 import 'package:untitled1/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp(hasWallets: false));
+    await tester.pumpWidget(ProviderScope(child: TranslationProvider(child: MyApp(hasWallets: false))));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
