fd4517bf89d57e27441e5735c373a2971d4e6b4a
Tyq_cool <896953946@qq.com>
2025-11-11 14:34:45 +0800

    test: update widget test for custom MyApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,6 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:untitled1/i18n/strings.g.dart';
@@ -17,16 +16,9 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(ProviderScope(child: TranslationProvider(child: MyApp(hasWallets: false))));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // 等一帧，确保不会抛异常就算过
+    await tester.pumpAndSettle();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(MyApp), findsOneWidget);
   });
 }
